5feab015bab9ba9792245e490ba01da6156560db
yaviles <yaviles@grupo-ari.net>
2019-04-24 13:57:14 -0400

    Modificacion de Querys PRe DOtacion

--- ajax/Add_dotacion_ficha.php
+++ ajax/Add_dotacion_ficha.php
@@ -9,9 +9,11 @@ $ficha = $_POST['cod_ficha'];
 $result = [];
 
 $sql = "SELECT  CONCAT(productos.descripcion,' (',productos.item,') ') producto, ficha_dotacion.cantidad,
-IFNULL(MAX(prod_dotacion.fec_us_mod),'SIN DOTACION') ult_dotacion
-FROM ficha_dotacion LEFT JOIN prod_dotacion on prod_dotacion.cod_ficha = ficha_dotacion.cod_ficha
-LEFT JOIN prod_dotacion_det on prod_dotacion.codigo = prod_dotacion_det.cod_dotacion AND prod_dotacion_det.cod_producto = ficha_dotacion.cod_item,
+IFNULL((SELECT MAX(prod_dotacion.fec_us_mod) FROM prod_dotacion, prod_dotacion_det
+WHERE prod_dotacion.codigo = prod_dotacion_det.cod_dotacion
+AND prod_dotacion_det.cod_producto = ficha_dotacion.cod_item
+AND prod_dotacion.cod_ficha = ficha_dotacion.cod_ficha) ,'SIN DOTACION') ult_dotacion
+FROM ficha_dotacion ,
 productos
 WHERE
 ficha_dotacion.cod_ficha = '$ficha'
--- formularios/add_prod_dotacion.php
+++ formularios/add_prod_dotacion.php
@@ -222,9 +222,11 @@ if($metodo == 'modificar'){
 	$activo        = $result["status"];
 
 	$sql = "SELECT  CONCAT(productos.descripcion,' (',productos.item,') ') producto, ficha_dotacion.cantidad,
-	IFNULL(MAX(prod_dotacion.fec_us_mod),'SIN DOTACION') ult_dotacion
-	FROM ficha_dotacion LEFT JOIN prod_dotacion on prod_dotacion.cod_ficha = ficha_dotacion.cod_ficha
-	LEFT JOIN prod_dotacion_det on prod_dotacion.codigo = prod_dotacion_det.cod_dotacion AND prod_dotacion_det.cod_producto = ficha_dotacion.cod_item,
+	IFNULL((SELECT MAX(prod_dotacion.fec_us_mod) FROM prod_dotacion, prod_dotacion_det
+	WHERE prod_dotacion.codigo = prod_dotacion_det.cod_dotacion
+	AND prod_dotacion_det.cod_producto = ficha_dotacion.cod_item
+	AND prod_dotacion.cod_ficha = ficha_dotacion.cod_ficha) ,'SIN DOTACION') ult_dotacion
+	FROM ficha_dotacion ,
 	productos
 	WHERE
 	ficha_dotacion.cod_ficha = '$ficha'
@@ -294,7 +296,7 @@ $proced      = "p_prod_dotacion";
 							</table>
 						</fieldset>
 						<fieldset class="fieldset" id="datos_dotacion">
-							<legend>Datos Dotacion: </legend>
+							<legend>Productos a Dotar: </legend>
 							<table width="100%" align="center" class="tabla_sistema">
 								<thead>
 									<tr>
