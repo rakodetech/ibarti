01d2ce52a4195aa499820966e097ec774858f7e0
yaviles <yaviles@grupo-ari.net>
2019-07-25 10:52:51 -0400

    correcion restricción cliente

--- reportes/rp_fic_trabajador.php
+++ reportes/rp_fic_trabajador.php
@@ -44,7 +44,10 @@ function Add_filtroX(){  // CARGAR  ARCHIVO DE AJAX CON UN PARAMETRO //
 		var error = error+1;
 		errorMessage = errorMessage + ' \n Debe Seleccionar un Rol ';
 	}
-
+	if(cliente == '') {
+		var error = error+1;
+		errorMessage = errorMessage + ' \n Debe Seleccionar un Cliente ';
+	}
 	if(error == 0){
 
 		var parametros = {
@@ -129,107 +132,109 @@ function Add_filtroX(){  // CARGAR  ARCHIVO DE AJAX CON UN PARAMETRO //
 						while($row01=$bd->obtener_fila($query01,0)){
 							echo '<option value="'.$row01[0].'">'.$row01[1].'</option>';
 						}?></select></td>
-																	<td>Cargo: </td>
-											<td><select name="cargo" id="cargo" style="width:120px;">
-												<option value="TODOS">TODOS</option>
-												<?php
-												$query01 = $bd->consultar($sql_cargo);
-												while($row01=$bd->obtener_fila($query01,0)){
-													echo '<option value="'.$row01[0].'">'.$row01[1].'</option>';
-												}?></select></td>
-						<td><?php echo $leng['estado']?>: </td>
-						<td><select name="estado" id="estado" style="width:120px;">
+						<td>Cargo: </td>
+						<td><select name="cargo" id="cargo" style="width:120px;">
 							<option value="TODOS">TODOS</option>
 							<?php
-							$query01 = $bd->consultar($sql_estado);
+							$query01 = $bd->consultar($sql_cargo);
 							while($row01=$bd->obtener_fila($query01,0)){
 								echo '<option value="'.$row01[0].'">'.$row01[1].'</option>';
 							}?></select></td>
-							<td><?php echo $leng['ciudad']?>: </td>
-							<td><select name="ciudad" id="ciudad" style="width:120px;">
+							<td><?php echo $leng['estado']?>: </td>
+							<td><select name="estado" id="estado" style="width:120px;">
 								<option value="TODOS">TODOS</option>
 								<?php
-								$query01 = $bd->consultar($sql_ciudad);
+								$query01 = $bd->consultar($sql_estado);
 								while($row01=$bd->obtener_fila($query01,0)){
 									echo '<option value="'.$row01[0].'">'.$row01[1].'</option>';
 								}?></select></td>
-						
-									</tr>
-									
-									<tr>
-								<td class="etiqueta"><?php echo $leng['cliente']?>:</td>
-								<td><select name="cliente" id="cliente" style="width:120px;" onchange="Add_Cl_Ubic(this.value, 'contenido_ubic', 'T', '120')">
-									<option value="TODOS"> TODOS</option>
-									<?php $query02 = $bd->consultar($sql_cliente);
-									while($row02=$bd->obtener_fila($query02,0)){
-										echo '<option value="'.$row02[0].'">'.$row02[1].'</option>';
+								<td><?php echo $leng['ciudad']?>: </td>
+								<td><select name="ciudad" id="ciudad" style="width:120px;">
+									<option value="TODOS">TODOS</option>
+									<?php
+									$query01 = $bd->consultar($sql_ciudad);
+									while($row01=$bd->obtener_fila($query01,0)){
+										echo '<option value="'.$row01[0].'">'.$row01[1].'</option>';
 									}?></select></td>
-										<td class="etiqueta"><?php echo $leng['ubicacion'];?>: </td>
-									<td id="contenido_ubic"><select name="ubicacion" id="ubicacion" style="width:120px;">
-										<option value="TODOS">TODOS</option>
-									</select></td>
-									<td>status: </td>
-									<td><select name="status" id="status" style="width:120px;">
-										<option value="TODOS">TODOS</option>
+
+								</tr>
+
+								<tr>
+									<td class="etiqueta"><?php echo $leng['cliente']?>:</td>
+									<td><select name="cliente" id="cliente" style="width:120px;" onchange="Add_Cl_Ubic(this.value, 'contenido_ubic', 'T', '120')">
 										<?php
-										$query01 = $bd->consultar($sql_ficha_status);
+										echo  $select_cl;
+										echo $sql_cliente;
+										$query01 = $bd->consultar($sql_cliente);
 										while($row01=$bd->obtener_fila($query01,0)){
 											echo '<option value="'.$row01[0].'">'.$row01[1].'</option>';
 										}?></select></td>
-										<td>&nbsp;</td>
-									</tr>
-									
+										<td class="etiqueta"><?php echo $leng['ubicacion'];?>: </td>
+										<td id="contenido_ubic"><select name="ubicacion" id="ubicacion" style="width:120px;">
+											<option value="TODOS">TODOS</option>
+										</select></td>
+										<td>status: </td>
+										<td><select name="status" id="status" style="width:120px;">
+											<option value="TODOS">TODOS</option>
+											<?php
+											$query01 = $bd->consultar($sql_ficha_status);
+											while($row01=$bd->obtener_fila($query01,0)){
+												echo '<option value="'.$row01[0].'">'.$row01[1].'</option>';
+											}?></select></td>
+											<td>&nbsp;</td>
+										</tr>
+
 										<tr>
 
 
-												<td>Filtro <?php echo $leng['trab']?>:</td>
-												<td id="select01">
-													<select id="paciFiltro" onchange="EstadoFiltro(this.value)" style="width:120px">
-														<option value="TODOS"> TODOS</option>
-														<option value="codigo"> <?php echo $leng['ficha']?> </option>
-														<option value="cedula"><?php echo $leng['ci']?> </option>
-														<option value="trabajador"><?php echo $leng['trabajador']?> </option>
-														<option value="nombres"> Nombre </option>
-														<option value="apellidos"> Apellido </option>
-													</select></td>
-													<td><?php echo $leng['trabajador']?>:</td>
-													<td colspan="2"><input  id="stdName" type="text" size="22" disabled="disabled" />
-														<input type="hidden" name="trabajador" id="stdID" value=""/></td>
-														<td>&nbsp;<input type="hidden" name="Nmenu" id="Nmenu" value="<?php echo $Nmenu;?>" />
-															<input type="hidden" name="mod" id="mod" value="<?php echo $mod;?>" />
-															<input type="hidden" name="archivo" id="archivo" value="<?php echo $archivo;?>" />
-															<input type="hidden" name="r_rol" id="r_rol" value="<?php echo $_SESSION['r_rol'];?>"/>
-															
-															<input type="hidden" name="r_cliente" id="r_cliente" valuee="<?php echo $_SESSION['r_cliente'];?>"/>
-															<input type="hidden" name="usuario" id="usuario" value="<?php echo $_SESSION['usuario_cod'];?>"/></td>
-														</tr>
-													</table><hr /><div id="listar">&nbsp;</div>
-													<div align="center"><br/>
-														<span class="art-button-wrapper">
-															<span class="art-button-l"> </span>
-															<span class="art-button-r"> </span>
-															<input type="button" name="salir" id="salir" value="Salir" onclick="Vinculo('inicio.php?area=formularios/index')"
-															class="readon art-button">
-														</span>&nbsp;
+											<td>Filtro <?php echo $leng['trab']?>:</td>
+											<td id="select01">
+												<select id="paciFiltro" onchange="EstadoFiltro(this.value)" style="width:120px">
+													<option value="TODOS"> TODOS</option>
+													<option value="codigo"> <?php echo $leng['ficha']?> </option>
+													<option value="cedula"><?php echo $leng['ci']?> </option>
+													<option value="trabajador"><?php echo $leng['trabajador']?> </option>
+													<option value="nombres"> Nombre </option>
+													<option value="apellidos"> Apellido </option>
+												</select></td>
+												<td><?php echo $leng['trabajador']?>:</td>
+												<td colspan="2"><input  id="stdName" type="text" size="22" disabled="disabled" />
+													<input type="hidden" name="trabajador" id="stdID" value=""/></td>
+													<td>&nbsp;<input type="hidden" name="Nmenu" id="Nmenu" value="<?php echo $Nmenu;?>" />
+														<input type="hidden" name="mod" id="mod" value="<?php echo $mod;?>" />
+														<input type="hidden" name="archivo" id="archivo" value="<?php echo $archivo;?>" />
+														<input type="hidden" name="r_rol" id="r_rol" value="<?php echo $_SESSION['r_rol'];?>"/>
 
-														<input type="submit" name="procesar" id="procesar" hidden="hidden">
-														<input type="text" name="reporte" id="reporte" hidden="hidden">
+														<input type="hidden" name="r_cliente" id="r_cliente" valuee="<?php echo $_SESSION['r_cliente'];?>"/>
+														<input type="hidden" name="usuario" id="usuario" value="<?php echo $_SESSION['usuario_cod'];?>"/></td>
+													</tr>
+												</table><hr /><div id="listar">&nbsp;</div>
+												<div align="center"><br/>
+													<span class="art-button-wrapper">
+														<span class="art-button-l"> </span>
+														<span class="art-button-r"> </span>
+														<input type="button" name="salir" id="salir" value="Salir" onclick="Vinculo('inicio.php?area=formularios/index')"
+														class="readon art-button">
+													</span>&nbsp;
 
-														<img class="imgLink" id="img_pdf" src="imagenes/pdf.gif" border="0"
-														onclick="{$('#reporte').val('pdf');$('#procesar').click();}" width="25px" title="imprimir a pdf">
+													<input type="submit" name="procesar" id="procesar" hidden="hidden">
+													<input type="text" name="reporte" id="reporte" hidden="hidden">
 
-														<img class="imgLink" id="img_excel" src="imagenes/excel.gif" border="0"
-														onclick="{$('#reporte').val('excel');$('#procesar').click();}" width="25px" title="imprimir a excel">
-													</div>
-												</form>
-												<script type="text/javascript">
-													r_cliente = $("#r_cliente").val();
-													r_rol     = $("#r_rol").val();
-													usuario   = $("#usuario").val();
-													filtroValue = $("#paciFiltro").val();
+													<img class="imgLink" id="img_pdf" src="imagenes/pdf.gif" border="0"
+													onclick="{$('#reporte').val('pdf');$('#procesar').click();}" width="25px" title="imprimir a pdf">
 
-													new Autocomplete("stdName", function() {
-														this.setValue = function(id) {
+													<img class="imgLink" id="img_excel" src="imagenes/excel.gif" border="0"
+													onclick="{$('#reporte').val('excel');$('#procesar').click();}" width="25px" title="imprimir a excel">
+												</div>
+											</form>
+											<script type="text/javascript">
+												r_cliente = $("#r_cliente").val();
+												r_rol     = $("#r_rol").val();
+												usuario   = $("#usuario").val();
+												filtroValue = $("#paciFiltro").val();
+
+												new Autocomplete("stdName", function() {
+													this.setValue = function(id) {
             document.getElementById("stdID").value = id; // document.getElementsByName("stdID")[0].value = id;
         }
         if (this.isModified) this.setValue("");
