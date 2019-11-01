0b2b57224ce887f0c27c0c462b8bb17aa4265ceb
luis <luis.gustavo.282000@gmail.com>
2019-11-01 10:01:24 -0400

    adicion de campo altitud y longitud en clientes ubicacion

--- packages/cliente/cl_ubicacion/controllers/ubicacionCtrl.js
+++ packages/cliente/cl_ubicacion/controllers/ubicacionCtrl.js
@@ -1,142 +1,156 @@
-$(function() {
+$(function () {
 	Cons_ubicacion_inicio();
 });
 
-function Cons_ubicacion_inicio(){
+function Cons_ubicacion_inicio() {
 
-		var cliente      = $("#c_codigo").val();
+	var cliente = $("#c_codigo").val();
 
-		var error        = 0;
-		var errorMessage = ' ';
-		  if(error == 0){
-		    var parametros = { "cliente":  cliente};
-		      $.ajax({
-		          data:  parametros,
-		          url:   'packages/cliente/cl_ubicacion/views/Cons_inicio.php',
-		          type:  'post',
-		          success:  function (response) {
-              $("#Cont_ubicacion").html(response);
-		          },
-		          error: function (xhr, ajaxOptions, thrownError) {
-		              alert(xhr.status);
-		              alert(thrownError);}
-		            });
-		  }else{
-		    alert(errorMessage);
-		  }
-}
-
-function Cons_ubic(cod, metodo, title){
-	ModalOpen();
-		$("#modal_title").text(title);
-		var usuario      = $("#usuario").val();
-		var cliente      = $("#c_codigo").val();
-
-		var error        = 0;
-		var errorMessage = ' ';
-		  if(error == 0){
-		    var parametros = { "codigo" : cod,      "cliente": cliente,
-				                   "metodo": metodo,    "usuario": usuario   };
-		      $.ajax({
-		          data:  parametros,
-		          url:   'packages/cliente/cl_ubicacion/views/Add_form.php',
-		          type:  'post',
-		          success:  function (response) {
-
-              $("#contenido_modal").html(response);
-							iniciar_tab(0);
-
-		          },
-		          error: function (xhr, ajaxOptions, thrownError) {
-		              alert(xhr.status);
-		              alert(thrownError);}
-		            });
-		  }else{
-		    alert(errorMessage);
-		  }
-	}
-
-function save_ubic(){
 	var error = 0;
 	var errorMessage = ' ';
-	var cliente     = $("#c_codigo").val();
-
-	var codigo     = $("#ub_codigo").val();
-	var status     = Status($("#ub_status:checked").val());
-	var nombre     = $("#ub_nombre").val();
-	var region     = $("#ub_region").val();
-	var zona       = $("#ub_zona").val();
-	var estado     = $("#ub_estado").val();
-	var ciudad     = $("#ub_ciudad").val();
-	var calendario = $("#ub_calendario").val();
-	var contacto   = $("#ub_contacto").val();
-	var cargo      = $("#ub_cargo").val();
-	var telefono   = $("#ub_telefono").val();
-	var email      = $("#ub_email").val();
-	var direccion  = $("#ub_direccion").val();
-	var observ     = $("#ub_observ").val();
-	var campo01    = $("#ub_campo01").val();
-	var campo02    = $("#ub_campo02").val();
-	var campo03    = $("#ub_campo03").val();
-	var campo04    = $("#ub_campo04").val();
-
-	var proced     = "p_clientes_ubic";
-	var usuario     = $("#usuario").val();
-	var metodo     = $("#ub_metodo").val();
-
-	if(error == 0){
-		var parametros = "X";
-		var parametros = {"codigo": codigo, 	  			"status": status,
-											"nombre": nombre,           "region" : region,
-											"estado": estado,           "ciudad": ciudad ,
-											"calendario":calendario ,   "zona":zona ,
-											"contacto": contacto ,
-											"cargo": cargo,             "telefono": telefono ,
-											"email": email,             "direccion": direccion ,
-											"observ":observ,
-											"proced": proced, 					"usuario": usuario,
-											"metodo":metodo,            "cliente": cliente,
-											"campo01":campo01,          "campo02":campo02 ,
-											"campo03":campo03, 					"campo04":campo04
-											};
-
-							$.ajax({
-									data:  parametros,
-									url:   'packages/cliente/cl_ubicacion/modelo/ubicacion.php',
-									type:  'post',
-									success:  function (response) {
-										CloseModal();
-											var cliente     = $("#c_codigo").val();
-											var metodo     = $("#c_metodo").val();
-											Cons_cliente(cliente, metodo);
-									},
-								error: function (xhr, ajaxOptions, thrownError) {
-										alert(xhr.status);
-										alert(thrownError);}
-							});
-		}else{
-			alert(errorMessage);
-		}
-	}
-
-function Borrar_ubic(cod){
-	var usuario      = $("#usuario").val();
-	var parametros = { "codigo" : cod,   "tabla" : "clientes_ubicacion",
-										 "usuario": usuario   };
+	if (error == 0) {
+		var parametros = { "cliente": cliente };
 		$.ajax({
-				data:  parametros,
-				url:   'packages/general/controllers/sc_borrar.php',
-				type:  'post',
-				success:  function (response) {
-				var resp = JSON.parse(response);
-				 if(resp.error){
-					alert(resp.mensaje);
-				 }else{
-					Cons_ubicacion_inicio();
-				 }
-				},
-				error: function (xhr, ajaxOptions, thrownError) {
-						alert(xhr.status);
-						alert(thrownError);}
-					});
+			data: parametros,
+			url: 'packages/cliente/cl_ubicacion/views/Cons_inicio.php',
+			type: 'post',
+			success: function (response) {
+				$("#Cont_ubicacion").html(response);
+			},
+			error: function (xhr, ajaxOptions, thrownError) {
+				alert(xhr.status);
+				alert(thrownError);
+			}
+		});
+	} else {
+		alert(errorMessage);
+	}
+}
+
+function Cons_ubic(cod, metodo, title) {
+	ModalOpen();
+	$("#modal_title").text(title);
+	var usuario = $("#usuario").val();
+	var cliente = $("#c_codigo").val();
+
+	var error = 0;
+	var errorMessage = ' ';
+	if (error == 0) {
+		var parametros = {
+			"codigo": cod, "cliente": cliente,
+			"metodo": metodo, "usuario": usuario
+		};
+		$.ajax({
+			data: parametros,
+			url: 'packages/cliente/cl_ubicacion/views/Add_form.php',
+			type: 'post',
+			success: function (response) {
+
+				$("#contenido_modal").html(response);
+				iniciar_tab(0);
+
+			},
+			error: function (xhr, ajaxOptions, thrownError) {
+				alert(xhr.status);
+				alert(thrownError);
+			}
+		});
+	} else {
+		alert(errorMessage);
+	}
+}
+
+function save_ubic() {
+	var error = 0;
+	var errorMessage = ' ';
+	var cliente = $("#c_codigo").val();
+
+	var codigo = $("#ub_codigo").val();
+	var status = Status($("#ub_status:checked").val());
+	var nombre = $("#ub_nombre").val();
+	var region = $("#ub_region").val();
+	var zona = $("#ub_zona").val();
+	var estado = $("#ub_estado").val();
+	var ciudad = $("#ub_ciudad").val();
+	var calendario = $("#ub_calendario").val();
+	var contacto = $("#ub_contacto").val();
+	var cargo = $("#ub_cargo").val();
+	var telefono = $("#ub_telefono").val();
+	var email = $("#ub_email").val();
+	var direccion = $("#ub_direccion").val();
+
+	var latitud = $("#ub_latitud").val();
+	var longitud = $("#ub_longitud").val();
+
+	var observ = $("#ub_observ").val();
+	var campo01 = $("#ub_campo01").val();
+	var campo02 = $("#ub_campo02").val();
+	var campo03 = $("#ub_campo03").val();
+	var campo04 = $("#ub_campo04").val();
+
+	var proced = "p_clientes_ubic";
+	var usuario = $("#usuario").val();
+	var metodo = $("#ub_metodo").val();
+
+	if (error == 0) {
+		var parametros = "X";
+		var parametros = {
+			"codigo": codigo, "status": status,
+			"nombre": nombre, "region": region,
+			"estado": estado, "ciudad": ciudad,
+			"calendario": calendario, "zona": zona,
+			"contacto": contacto,
+			"cargo": cargo, "telefono": telefono,
+			"email": email, "direccion": direccion,
+			"latitud": latitud, "longitud": longitud,
+			"observ": observ,
+			"proced": proced, "usuario": usuario,
+			"metodo": metodo, "cliente": cliente,
+			"campo01": campo01, "campo02": campo02,
+			"campo03": campo03, "campo04": campo04
+		};
+		console.log(parametros);
+		$.ajax({
+			data: parametros,
+			url: 'packages/cliente/cl_ubicacion/modelo/ubicacion.php',
+			type: 'post',
+			success: function (response) {
+				CloseModal();
+				var cliente = $("#c_codigo").val();
+				var metodo = $("#c_metodo").val();
+				Cons_cliente(cliente, metodo);
+			},
+			error: function (xhr, ajaxOptions, thrownError) {
+				alert(xhr.status);
+				alert(thrownError);
+			}
+		});
+	} else {
+		alert(errorMessage);
+	}
+}
+
+function Borrar_ubic(cod) {
+	var usuario = $("#usuario").val();
+	var parametros = {
+		"codigo": cod, "tabla": "clientes_ubicacion",
+		"usuario": usuario
+	};
+	$.ajax({
+		data: parametros,
+		url: 'packages/general/controllers/sc_borrar.php',
+		type: 'post',
+		success: function (response) {
+			var resp = JSON.parse(response);
+			if (resp.error) {
+				alert(resp.mensaje);
+			} else {
+				Cons_ubicacion_inicio();
+			}
+		},
+		error: function (xhr, ajaxOptions, thrownError) {
+			alert(xhr.status);
+			alert(thrownError);
+		}
+	});
 }
--- packages/cliente/cl_ubicacion/views/p_ubic.php
+++ packages/cliente/cl_ubicacion/views/p_ubic.php
@@ -1,126 +1,139 @@
 <script language="javascript">
-$("#ub_form").on('submit', function(evt){
-	 evt.preventDefault();
-	 save_ubic();
-});
+	$("#ub_form").on('submit', function(evt) {
+		evt.preventDefault();
+		save_ubic();
+	});
 </script>
 <form id="ub_form" name="ub_form" method="post">
-<fieldset class="fieldset">
-  <legend><?php echo $titulo;?></legend>
-     <table width="90%" align="center">
-    <tr>
-      <td width="15%" class="etiqueta">C&oacute;digo:</td>
-      <td width="35%"><input type="text" id="ub_codigo" maxlength="11" size="15" value="<?php echo $ubic["codigo"];?>" readonly  />
-               Activo: <input id="ub_status" type="checkbox"  <?php echo statusCheck($ubic["status"]);?> value="T" />
-      </td>
-      <td width="15%" class="etiqueta"><?php echo $leng['ubicacion']?>: </td>
-      <td width="35%"><input type="text" id="ub_nombre" maxlength="30" size="30" required value="<?php echo $ubic["descripcion"];?>"/>
-      </td>
-    </tr>
+	<fieldset class="fieldset">
+		<legend><?php echo $titulo; ?></legend>
+		<table width="90%" align="center">
+			<tr>
+				<td width="15%" class="etiqueta">C&oacute;digo:</td>
+				<td width="35%"><input type="text" id="ub_codigo" maxlength="11" size="15" value="<?php echo $ubic["codigo"]; ?>" readonly />
+					Activo: <input id="ub_status" type="checkbox" <?php echo statusCheck($ubic["status"]); ?> value="T" />
+				</td>
+				<td width="15%" class="etiqueta"><?php echo $leng['ubicacion'] ?>: </td>
+				<td width="35%"><input type="text" id="ub_nombre" maxlength="30" size="30" required value="<?php echo $ubic["descripcion"]; ?>" />
+				</td>
+			</tr>
 
-	  <tr>
-      <td class="etiqueta"><?php echo $leng['region']?>:</td>
-      	<td><select id="ub_region" style="width:250px" required>
-							<option value="<?php echo $ubic["cod_region"];?>"><?php echo $ubic["region"];?></option>
-          	<?php
-					    foreach ($region as  $datos) {
-          			echo '<option value="'.$datos[0].'">'.$datos[1].'</option>';
-           	}?>
-        </select></td>
-      <td class="etiqueta"><?php echo $leng['estado']?>: </td>
-      	<td><select id="ub_estado" required style="width:250px" onchange="Filtrar_select(this.value, 'ub_ciudad', 'ajax/Add_select_ciudad.php', 'ciudad', '250px', '')">
-							<option value="<?php echo $ubic["cod_estado"];?>"><?php echo $ubic["estado"];?></option>
-							<?php
-								foreach ($estado as $datos) {
-									echo '<option value="'.$datos[0].'">'.$datos[1].'</option>';
-							}?>
-        </select><br /><span class="selectRequiredMsg">Debe Seleccionar Un Campo.</span></td>
-	 </tr>
+			<tr>
+				<td class="etiqueta"><?php echo $leng['region'] ?>:</td>
+				<td><select id="ub_region" style="width:250px" required>
+						<option value="<?php echo $ubic["cod_region"]; ?>"><?php echo $ubic["region"]; ?></option>
+						<?php
+						foreach ($region as  $datos) {
+							echo '<option value="' . $datos[0] . '">' . $datos[1] . '</option>';
+						} ?>
+					</select></td>
+				<td class="etiqueta"><?php echo $leng['estado'] ?>: </td>
+				<td><select id="ub_estado" required style="width:250px" onchange="Filtrar_select(this.value, 'ub_ciudad', 'ajax/Add_select_ciudad.php', 'ciudad', '250px', '')">
+						<option value="<?php echo $ubic["cod_estado"]; ?>"><?php echo $ubic["estado"]; ?></option>
+						<?php
+						foreach ($estado as $datos) {
+							echo '<option value="' . $datos[0] . '">' . $datos[1] . '</option>';
+						} ?>
+					</select><br /><span class="selectRequiredMsg">Debe Seleccionar Un Campo.</span></td>
+			</tr>
 
-    <tr>
-      <td class="etiqueta"><?php echo $leng['ciudad']?>:</td>
-      <td id="ciudad"><select id="ub_ciudad" style="width:250px"  required>
-							<option value="<?php echo $ubic["cod_ciudad"];?>"><?php echo $ubic["ciudad"];?></option>
-							<?php
-								foreach ($ciudad as $datos) {
-									echo '<option value="'.$datos[0].'">'.$datos[1].'</option>';
-							}?>
-        </select></td>
+			<tr>
+				<td class="etiqueta"><?php echo $leng['ciudad'] ?>:</td>
+				<td id="ciudad"><select id="ub_ciudad" style="width:250px" required>
+						<option value="<?php echo $ubic["cod_ciudad"]; ?>"><?php echo $ubic["ciudad"]; ?></option>
+						<?php
+						foreach ($ciudad as $datos) {
+							echo '<option value="' . $datos[0] . '">' . $datos[1] . '</option>';
+						} ?>
+					</select></td>
 				<td class="etiqueta">Zona:</td>
-				<td id="ciudad"><select id="ub_zona" style="width:250px"  required>
-								<option value="<?php echo $ubic["cod_zona"];?>"><?php echo $ubic["zona"];?></option>
-								<?php
-									foreach ($zona as $datos) {
-										echo '<option value="'.$datos[0].'">'.$datos[1].'</option>';
-								}?>
+				<td id="ciudad"><select id="ub_zona" style="width:250px" required>
+						<option value="<?php echo $ubic["cod_zona"]; ?>"><?php echo $ubic["zona"]; ?></option>
+						<?php
+						foreach ($zona as $datos) {
+							echo '<option value="' . $datos[0] . '">' . $datos[1] . '</option>';
+						} ?>
 					</select></td>
-    </tr>
-		<tr>
+			</tr>
+			<tr>
 				<td class="etiqueta">Calendario:</td>
-				<td id="ciudad"><select id="ub_calendario" style="width:250px"  required>
-								<option value="<?php echo $ubic["cod_calendario"];?>"><?php echo $ubic["calendario"];?></option>
-								<?php
-									foreach ($calendario as $datos) {
-										echo '<option value="'.$datos[0].'">'.$datos[1].'</option>';
-								}?>
+				<td id="ciudad"><select id="ub_calendario" style="width:250px" required>
+						<option value="<?php echo $ubic["cod_calendario"]; ?>"><?php echo $ubic["calendario"]; ?></option>
+						<?php
+						foreach ($calendario as $datos) {
+							echo '<option value="' . $datos[0] . '">' . $datos[1] . '</option>';
+						} ?>
 					</select></td>
-		</tr>
-    <tr>
-      <td class="etiqueta">Contacto: </td>
-      <td id="input03"><input type="text" id="ub_contacto" maxlength="30" size="30" value="<?php echo $ubic["contacto"];?>" required/><br />
-		   <span class="textfieldRequiredMsg">El Campo es Requerido...</span>
-      </td>
+			</tr>
+			<tr>
+				<td class="etiqueta">Contacto: </td>
+				<td id="input03"><input type="text" id="ub_contacto" maxlength="30" size="30" value="<?php echo $ubic["contacto"]; ?>" required /><br />
+					<span class="textfieldRequiredMsg">El Campo es Requerido...</span>
+				</td>
 
-      <td class="etiqueta">Cargo: </td>
-      <td id="input04"><input type="text" id="ub_cargo" maxlength="30" size="30" value="<?php echo $ubic["cargo"];?>" required/>
-      </td>
-    </tr>
-    <tr>
-      <td class="etiqueta">Tel&eacute;fono: </td>
-      <td id="input05"><input type="text" id="ub_telefono" maxlength="60" size="30" value="<?php echo $ubic["telefono"];?>"/>
-      </td>
+				<td class="etiqueta">Cargo: </td>
+				<td id="input04"><input type="text" id="ub_cargo" maxlength="30" size="30" value="<?php echo $ubic["cargo"]; ?>" required />
+				</td>
+			</tr>
+			<tr>
 
-      <td class="etiqueta"><?php echo $leng['correo']?>: </td>
-      <td><input  type="email" id="ub_email" maxlength="60" size="30" value="<?php echo $ubic["email"];?>"/></td>
-    </tr>
-    <tr>
-      <td class="etiqueta">Direcci&oacute;n:</td>
-      <td id="textarea01"><textarea  id="ub_direccion" cols="50" rows="3"><?php echo $ubic["direccion"];?></textarea>
-        <span id="Counterror_mess2" class="texto">&nbsp;</span><br />
-        <span class="textareaRequiredMsg">El Campo es Requerido.</span>
-        <span class="textareaMinCharsMsg">Debe Escribir mas de 4 caracteres.</span>
-        <span class="textareaMaxCharsMsg">El maximo de caracteres permitidos es 300.</span></td>
+				<td class="etiqueta">Tel&eacute;fono: </td>
+				<td id="input05"><input type="text" id="ub_telefono" maxlength="60" size="30" value="<?php echo $ubic["telefono"]; ?>" />
+				</td>
 
-      <td class="etiqueta">Observación:</td>
-      <td id="textarea02"><textarea  id="ub_observ" cols="50" rows="3"><?php echo $ubic["observacion"];?></textarea>
-        <span id="Counterror_mess2" class="texto">&nbsp;</span><br />
-        <span class="textareaRequiredMsg">El Campo es Requerido.</span>
-        <span class="textareaMinCharsMsg">Debe Escribir mas de 4 caracteres.</span>
-        <span class="textareaMaxCharsMsg">El maximo de caracteres permitidos es 300.</span></td>
-    </tr>
-	 <tr>
-         <td height="8" colspan="4" align="center"><hr></td>
-     </tr>
+				<td class="etiqueta"><?php echo $leng['correo'] ?>: </td>
+				<td><input type="email" id="ub_email" maxlength="60" size="30" value="<?php echo $ubic["email"]; ?>" /></td>
+			</tr>
+			<tr>
+				<td class="etiqueta">Direcci&oacute;n:</td>
+				<td id="textarea01"><textarea id="ub_direccion" cols="50" rows="3"><?php echo $ubic["direccion"]; ?></textarea>
+					<span id="Counterror_mess2" class="texto">&nbsp;</span><br />
+					<span class="textareaRequiredMsg">El Campo es Requerido.</span>
+					<span class="textareaMinCharsMsg">Debe Escribir mas de 4 caracteres.</span>
+					<span class="textareaMaxCharsMsg">El maximo de caracteres permitidos es 300.</span></td>
 
-  </table>
-	<div align="center"><br/>
-		 <span class="art-button-wrapper">
-		 			 <span class="art-button-l"> </span>
-		 			 <span class="art-button-r"> </span>
-		 			<input type="submit" name="salvar"  id="salvar" value="Guardar" class="readon art-button" />
-		 	 </span>&nbsp;
+				<td class="etiqueta">Observación:</td>
+				<td id="textarea02"><textarea id="ub_observ" cols="50" rows="3"><?php echo $ubic["observacion"]; ?></textarea>
+					<span id="Counterror_mess2" class="texto">&nbsp;</span><br />
+					<span class="textareaRequiredMsg">El Campo es Requerido.</span>
+					<span class="textareaMinCharsMsg">Debe Escribir mas de 4 caracteres.</span>
+					<span class="textareaMaxCharsMsg">El maximo de caracteres permitidos es 300.</span></td>
+			</tr>
+			<tr>
+				<td class="etiqueta">Latitud: </td>
+				<td id="input03"><input type="text" id="ub_latitud" maxlength="30" size="30" value="<?php echo $ubic["latitud"]; ?>" required /><br />
+					<span class="textfieldRequiredMsg">El Campo es Requerido...</span>
+				</td>
+				<td class="etiqueta">Longitud: </td>
+				<td id="input04"><input type="text" id="ub_longitud" maxlength="30" size="30" value="<?php echo $ubic["longitud"]; ?>" required />
+				</td>
 
-	<span class="art-button-wrapper">
-				 <span class="art-button-l"> </span>
-				 <span class="art-button-r"> </span>
-		 <input type="reset" id="limpiar" value="Restablecer" class="readon art-button" />
-		 </span>&nbsp;
-	<span class="art-button-wrapper">
-				 <span class="art-button-l"> </span>
-				 <span class="art-button-r"> </span>
-		 <input type="button" id="volver" value="Cerrar" onClick="CloseModal();" class="readon art-button" />
-		 </span>
-	</div>
-	 <input name="metodo" id="ub_metodo" type="hidden"  value="<?php echo $metodo;?>" />
-  </fieldset>
+			</tr>
+			<tr>
+				<td height="8" colspan="4" align="center">
+					<hr>
+				</td>
+			</tr>
+
+		</table>
+		<div align="center"><br />
+			<span class="art-button-wrapper">
+				<span class="art-button-l"> </span>
+				<span class="art-button-r"> </span>
+				<input type="submit" name="salvar" id="salvar" value="Guardar" class="readon art-button" />
+			</span>&nbsp;
+
+			<span class="art-button-wrapper">
+				<span class="art-button-l"> </span>
+				<span class="art-button-r"> </span>
+				<input type="reset" id="limpiar" value="Restablecer" class="readon art-button" />
+			</span>&nbsp;
+			<span class="art-button-wrapper">
+				<span class="art-button-l"> </span>
+				<span class="art-button-r"> </span>
+				<input type="button" id="volver" value="Cerrar" onClick="CloseModal();" class="readon art-button" />
+			</span>
+		</div>
+		<input name="metodo" id="ub_metodo" type="hidden" value="<?php echo $metodo; ?>" />
+	</fieldset>
 </form>
