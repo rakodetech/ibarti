cd1b13c150409d8648ce3ee22798b8f0b02d4778
luis <luis.gustavo.282000@gmail.com>
2019-11-06 14:08:01 -0400

    validacion de codigo y login en creacion de usuarios

--- autocompletar/tb/usuario.php
+++ autocompletar/tb/usuario.php
@@ -1,6 +1,6 @@
-<?php   
+<?php
 //   require('../../autentificacion/aut_config.inc.php');
- //    mysql_select_db($bd_cnn,$cnn);
+//    mysql_select_db($bd_cnn,$cnn);
 /*
 SELECT men_usuarios.codigo, men_usuarios.cedula,
 men_usuarios.nombre, men_usuarios.apellido,
@@ -10,39 +10,57 @@ FROM men_usuarios
 
 include_once('../../funciones/funciones.php');
 require("../../autentificacion/aut_config.inc.php");
-require_once("../../".class_bd);
+require_once("../../" . class_bd);
 $bd = new DataBase();
-	$typing     = $_GET['q'];
-	$filtro     = $_GET['filtro'];
-	$where  = " ";
-	switch ($filtro){	
-		case 'cedula':
-		   $where  .= " WHERE LOCATE('$typing',  men_usuarios.cedula) "; 
-        break;
-		case 'nombre':
-		   $where  .= " WHERE LOCATE('$typing', men_usuarios.nombres) "; 		  
-	 break;
-		case 'apellido':
-		   $where  .= " WHERE LOCATE('$typing',  men_usuarios.apellido) "; 
-        break;		
-	}	
+$typing     = $_GET['q'];
+$filtro     = $_GET['filtro'];
+$where  = " ";
+switch ($filtro) {
+	case 'cedula':
+		$where  .= " WHERE LOCATE('$typing',  men_usuarios.cedula) ";
+		break;
+	case 'nombre':
+		$where  .= " WHERE LOCATE('$typing', men_usuarios.nombres) ";
+		break;
+	case 'apellido':
+		$where  .= " WHERE LOCATE('$typing',  men_usuarios.apellido) ";
+		break;
 
-    $sql ="SELECT men_usuarios.codigo, men_usuarios.cedula,
-                  men_usuarios.nombre, men_usuarios.apellido
+		//////////
+	case 'codigo':
+		$where  .= " WHERE  men_usuarios.codigo = '$typing' ";
+		break;
+	case 'login':
+		$where  .= " WHERE  men_usuarios.login = '$typing' ";
+		break;
+		//////
+}
+if ($filtro == 'codigo' || $filtro == 'login') {
+	$sql = "SELECT COUNT(men_usuarios.codigo)
              FROM men_usuarios 
-		   ".$where." ORDER BY apellido DESC"; 
+		   " . $where . " ORDER BY apellido DESC";
+	$query = $bd->consultar($sql);
 
-   $query = $bd->consultar($sql);
+	while ($datos = $bd->obtener_fila($query, 0)) {
+		echo $datos[0];
+	}
+} else {
+	$sql = "SELECT men_usuarios.codigo, men_usuarios.cedula,
+					  men_usuarios.nombre, men_usuarios.apellido
+				 FROM men_usuarios 
+			   " . $where . " ORDER BY apellido DESC";
 
-	while ($datos=$bd->obtener_fila($query,0)){
- 			
+	$query = $bd->consultar($sql);
 
-	$descripcion = $datos['apellido']." &nbsp; ".$datos['nombre']." &nbsp;(".$datos['cedula'].")"; 
-	$cod          = $datos[0];
-?>
+	while ($datos = $bd->obtener_fila($query, 0)) {
+		$descripcion = $datos['apellido'] . " &nbsp; " . $datos['nombre'] . " &nbsp;(" . $datos['cedula'] . ")";
+		$cod          = $datos[0];
 
-<li onselect=" this.setText('<?php echo $descripcion?>').setValue('<?php echo  $cod ?>') ">
-  <b></b> <?php echo $descripcion?>
+		?>
 
-</li>
-<?php }?>
+			<li onselect=" this.setText('<?php echo $descripcion ?>').setValue('<?php echo  $cod ?>') ">
+				<b></b> <?php echo $descripcion ?>
+
+			</li>
+	<?php }
+	} ?>
