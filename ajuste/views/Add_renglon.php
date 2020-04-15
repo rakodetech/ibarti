d25eaba8877d17e5de381131ac0a846cd697bce9
IBARTI2019 <ibartisoftware@gmail.com>
2020-04-15 11:16:31 -0400

    Correccion
    Carpeta adicional

--- ajuste/views/Add_renglon.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "../modelo/ajuste_modelo.php";
-require "../../../../".Leng;
-$ajuste = new Ajuste;
-$codigo = $_POST['codigo'];
-$reng   = $ajuste->get_aj_reng($codigo);
-print_r(json_encode($reng));
-return json_encode($reng);
-?>
