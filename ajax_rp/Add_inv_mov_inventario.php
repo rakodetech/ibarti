f7cc0c2dedd7ddadf14eaebf0ed7be7d890527b3
Yonathan Aviles <yonathanaviles123@gmail.com>
2020-12-11 13:44:44 -0400

    Update Add_inv_mov_inventario.php

--- ajax_rp/Add_inv_mov_inventario.php
+++ ajax_rp/Add_inv_mov_inventario.php
@@ -2,8 +2,8 @@
 define("SPECIALCONSTANT", true);
 include_once "../funciones/funciones.php";
 require "../autentificacion/aut_config.inc.php";
-require "../".class_bdI;
-require "../".Leng;
+require "../" . class_bdI;
+require "../" . Leng;
 $bd = new DataBase();
 
 $fecha_D   = conversion($_POST['fecha_desde']);
@@ -19,31 +19,61 @@ AND ajuste.codigo = ajuste_reng.cod_ajuste AND ajuste.cod_tipo = prod_mov_tipo.c
 AND prod_sub_lineas.codigo = productos.cod_sub_linea AND tallas.codigo = productos.cod_talla
 AND ajuste.fecha BETWEEN '$fecha_D' AND '$fecha_H' ";
 
-if($almacen != "TODOS"){
+$where_alcance = " WHERE ajuste_alcance.codigo =ajuste_alcance_reng.cod_ajuste
+AND ajuste_alcance_reng.cod_almacen = almacenes.codigo
+ANd ajuste_alcance_reng.cod_producto = productos.item
+AND ajuste_alcance.fecha BETWEEN '$fecha_D' AND '$fecha_H' ";
+
+if ($almacen != "TODOS") {
 	$where .= " AND ajuste_reng.cod_almacen = '$almacen' ";
+	$where_alcance .= " AND ajuste_alcance_reng.cod_almacen = '$almacen' ";
 }
 
-if($producto != "TODOS"){
+if ($producto != "TODOS") {
 	$where .= " AND ajuste_reng.cod_producto = '$producto' ";
+	$where_alcance .= " AND ajuste_alcance_reng.cod_producto = '$producto' ";
 }
 
-if($tipo != "TODOS"){
+if ($tipo != "TODOS") {
 	$where .= " AND ajuste.cod_tipo= '$tipo' ";
 }
 
-if($referencia != "" && $referencia != null){
+if ($referencia != "" && $referencia != null) {
 	$where .= " AND ajuste.referencia= '$referencia' ";
 }
 
 $sql = " SELECT ajuste.codigo,ajuste.referencia,ajuste.fecha,prod_mov_tipo.descripcion ajuste,prod_mov_tipo.tipo_movimiento,almacenes.codigo cod_almacen, almacenes.descripcion almacen,productos.item cod_producto, IF(prod_sub_lineas.talla = 'T', CONCAT(productos.descripcion,' ',tallas.descripcion), productos.descripcion ) producto ,ajuste_reng.cantidad,ajuste_reng.costo,ajuste_reng.neto,
 ajuste_reng.cant_acum,ajuste_reng.importe importe_acum,ajuste_reng.cos_promedio ,ajuste_reng.aplicar
 FROM ajuste,ajuste_reng,prod_mov_tipo,almacenes,productos,prod_sub_lineas,tallas
-$where
-ORDER BY ajuste.fecha,ajuste_reng.cod_ajuste, ajuste_reng.reng_num  ASC ";
+$where ";
 
+if ($tipo == "TODOS" || $tipo == 'DOT') {
+	$sql .= " UNION
+		SELECT
+			ajuste_alcance.codigo,
+			ajuste_alcance.referencia,
+			ajuste_alcance.fecha,
+			'DOTACION ALCANCE',
+			'OUT',
+			ajuste_alcance_reng.cod_almacen,
+			almacenes.descripcion,
+			ajuste_alcance_reng.cod_producto,
+			productos.descripcion producto, 
+			ajuste_alcance_reng.cantidad,
+		0 costo,
+		0 neto,
+		0 cant_acum,
+		0 importe_acum,
+		0 cos_promedio,
+		'OUT' aplicar
+		FROM ajuste_alcance, ajuste_alcance_reng, almacenes, productos
+		$where_alcance ";
+}
+
+$sql .= " ORDER BY fecha ASC ";
 //echo $sql;
 $query = $bd->consultar($sql);
-while($rows=$bd->obtener_name($query)){
+while ($rows = $bd->obtener_name($query)) {
 	$result[] = $rows;
 }
 
@@ -58,7 +88,7 @@ while($rows=$bd->obtener_name($query)){
 		<th width="8%" class="etiqueta">Fecha Hora</th>
 		<th width="13%" class="etiqueta">Tipo Movimiento</th>
 		<th width="13%" class="etiqueta">Almacen</th>
-		<th width="18%" class="etiqueta"><?php echo $leng['producto']?></th>
+		<th width="18%" class="etiqueta"><?php echo $leng['producto'] ?></th>
 		<th width="8%" class="etiqueta">Cantidad</th>
 		<th width="8%" class="etiqueta">Costo</th>
 		<th width="8%" class="etiqueta">Neto</th>
@@ -69,26 +99,26 @@ while($rows=$bd->obtener_name($query)){
 	<?php
 	$valor = 0;
 	$query = $bd->consultar($sql);
-	while ($datos=$bd->obtener_fila($query,0)){
+	while ($datos = $bd->obtener_fila($query, 0)) {
 		$signo = "";
-		if($datos["aplicar"]=="IN"){
+		if ($datos["aplicar"] == "IN") {
 			$signo = "+";
-		}elseif($datos["aplicar"]=="OUT"){
-				$signo = "-";
+		} elseif ($datos["aplicar"] == "OUT") {
+			$signo = "-";
 		}
 		echo '<tr>
-		<td class="texto">'.$datos["codigo"].'</td>
-		<td class="texto">'.$datos["referencia"].'</td>
-		<td class="texto">'.$datos["fecha"].'</td>
-		<td class="texto">'.longitud($datos["ajuste"]).'</td>
-		<td class="texto">'.longitud($datos["almacen"]).'</td>
-		<td class="texto">'.$datos["producto"].'</td>
-		<td class="texto">'.$signo.''.$datos["cantidad"].'</td>
-		<td class="texto">'.$datos["costo"].'</td>
-		<td class="texto">'.$datos["neto"].'</td>
-		<td class="texto">'.$datos["cant_acum"].'</td>
-		<td class="texto">'.$datos["importe_acum"].'</td>
-		<td class="texto">'.$datos["cos_promedio"].'</td>
+		<td class="texto">' . $datos["codigo"] . '</td>
+		<td class="texto">' . $datos["referencia"] . '</td>
+		<td class="texto">' . $datos["fecha"] . '</td>
+		<td class="texto">' . longitud($datos["ajuste"]) . '</td>
+		<td class="texto">' . longitud($datos["almacen"]) . '</td>
+		<td class="texto">' . $datos["producto"] . '</td>
+		<td class="texto">' . $signo . '' . $datos["cantidad"] . '</td>
+		<td class="texto">' . $datos["costo"] . '</td>
+		<td class="texto">' . $datos["neto"] . '</td>
+		<td class="texto">' . $datos["cant_acum"] . '</td>
+		<td class="texto">' . $datos["importe_acum"] . '</td>
+		<td class="texto">' . $datos["cos_promedio"] . '</td>
 		</tr>';
-	};?>
+	}; ?>
 </table>
