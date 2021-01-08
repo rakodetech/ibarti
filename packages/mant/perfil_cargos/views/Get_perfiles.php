6e0ffa84aa2ab1a650ba305b84b21cc562167f97
Yonathan Aviles <yonathanaviles123@gmail.com>
2021-01-08 00:50:05 -0400

    Update Get_perfiles.php

--- packages/mant/perfil_cargos/views/Get_perfiles.php
+++ packages/mant/perfil_cargos/views/Get_perfiles.php
@@ -8,6 +8,6 @@ $perfiles = $modelo->get_perfiles();
 echo '<select id="perfil" name="perfil" style="width:300px" onchange="Cons_perfil_form(this.value)">
        <option value="">Seleccione...</option>';
 foreach ($perfiles as  $datos) {
-       echo '<option value="' . $datos[0] . '">' . $datos[1] . '<option>';
+       echo '<option value="' . $datos[0] . '">' . $datos[1] . '</option>';
 }
 echo '</select>';
