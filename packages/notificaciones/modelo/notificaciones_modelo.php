6c4d00d1be69991992701bbf57cbc598da6c319d
Yonathan Aviles <yonathanaviles123@gmail.com>
2021-02-24 15:50:07 -0400

    Filtro por estado y ciudad en campana de notificaciones

--- packages/notificaciones/modelo/notificaciones_modelo.php
+++ packages/notificaciones/modelo/notificaciones_modelo.php
@@ -22,17 +22,19 @@ class Notificaciones
 	public function get_nov_notif($perfil, $usuario)
 	{
 
-		$sql_region = "SELECT men_usuarios.cod_region FROM men_usuarios
+		$sql_region = "SELECT men_usuarios.cod_region, men_usuarios.cod_estado, men_usuarios.cod_ciudad FROM men_usuarios
 		WHERE  men_usuarios.codigo = '$usuario';";
 
 		$query_region        = $this->bd->consultar($sql_region);
 		$data = $this->bd->obtener_fila($query_region);
 
 		$region = $data[0];
+		$estado = $data[1];
+		$ciudad = $data[2];
 
 		$where = " where nov_procesos.cod_novedad = novedades.codigo
 		AND nov_status.control_notificaciones = 'T'
-		and nov_procesos.fec_us_mod > DATE_SUB(DATE_FORMAT(CURDATE(), '%Y-%m-%d'), INTERVAL control.dias_nov_notif DAY) 
+		-- and nov_procesos.fec_us_mod > DATE_SUB(DATE_FORMAT(CURDATE(), '%Y-%m-%d'), INTERVAL control.dias_nov_notif DAY) 
 		and men_usuarios.codigo = nov_procesos.cod_us_mod
 		and nov_status.codigo = nov_procesos.cod_nov_status
 		and nov_procesos_det.cod_nov_proc = nov_procesos.codigo
@@ -47,6 +49,14 @@ class Notificaciones
 
 		if ($region != null && $region != '') {
 			$where .= " AND clientes_ubicacion.cod_region = '$region' ";
+		}
+
+		if ($estado != null && $estado != '') {
+			$where .= " AND clientes_ubicacion.cod_estado = '$estado' ";
+		}
+
+		if ($ciudad != null && $ciudad != '') {
+			$where .= " AND clientes_ubicacion.cod_ciudad = '$ciudad' ";
 		}
 
 		$sql = "SELECT nov_procesos.codigo,nov_procesos_det.codigo cod_proc,nov_procesos_det.observacion ,ficha.cedula,nov_procesos.fec_us_mod fecha,novedades.descripcion,men_usuarios.nombre,nov_status.descripcion as stat,nov_status.color_notificaciones as color
@@ -64,13 +74,15 @@ class Notificaciones
 
 	public function get_nov_reciente($perfil, $usuario)
 	{
-		$sql_region = "SELECT nov_procesos.cod_region FROM men_usuarios
+		$sql_region = "SELECT nov_procesos.cod_region, men_usuarios.cod_estado, men_usuarios.cod_ciudad FROM men_usuarios
 		WHERE  nov_procesos_det.cod_us_ing <> '$usuario';";
 
 		$query_region        = $this->bd->consultar($sql_region);
 		$data = $this->bd->obtener_fila($query_region);
 
 		$region = $data[0];
+		$estado = $data[1];
+		$ciudad = $data[2];
 
 		$where = " WHERE  nov_procesos.cod_novedad = novedades.codigo and nov_procesos_det.cod_nov_proc = nov_procesos.codigo AND novedades.`status` = 'T' AND 
 		(TIME_TO_SEC(nov_procesos_det.hora) > (TIME_TO_SEC(CURRENT_TIME()) - (control.min_nov_notif * 60))) 
@@ -87,6 +99,15 @@ class Notificaciones
 		if ($region != null && $region != '') {
 			$where .= " AND clientes_ubicacion.cod_region = '$region' ";
 		}
+
+		if ($estado != null && $estado != '') {
+			$where .= " AND clientes_ubicacion.cod_estado = '$estado' ";
+		}
+
+		if ($ciudad != null && $ciudad != '') {
+			$where .= " AND clientes_ubicacion.cod_ciudad = '$ciudad' ";
+		}
+
 
 		$sql = "SELECT nov_procesos.codigo,men_usuarios.cedula,nov_procesos_det.observacion,nov_procesos_det.fec_us_ing fecha,novedades.descripcion,men_usuarios.nombre,nov_status.descripcion as stat,nov_status.color_notificaciones as color
 		FROM nov_procesos,nov_procesos_det,novedades,nov_perfiles, men_usuarios, control, nov_status, clientes_ubicacion
