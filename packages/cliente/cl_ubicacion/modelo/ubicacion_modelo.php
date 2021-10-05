1cdc69cafb93e1bd7cc73afdc4721d7657f3d5ff
Yonathan Aviles <yonathanaviles123@gmail.com>
2021-10-05 10:57:10 -0400

    Update ubicacion_modelo.php

--- packages/cliente/cl_ubicacion/modelo/ubicacion_modelo.php
+++ packages/cliente/cl_ubicacion/modelo/ubicacion_modelo.php
@@ -2,19 +2,20 @@
 define("SPECIALCONSTANT", true);
 include_once('../../../../funciones/funciones.php');
 require("../../../../autentificacion/aut_config.inc.php");
-require_once("../../../../".class_bdI);
+require_once("../../../../" . class_bdI);
 
 class Ubicacion
 {
-	 private $datos;
-	 private $bd;
+	private $datos;
+	private $bd;
 
 	function __construct()
 	{
-    $this->bd = new Database;
+		$this->bd = new Database;
 	}
 
-	public function get($cliente){
+	public function get($cliente)
+	{
 		$this->datos   = array();
 
 		$sql = " SELECT clientes_ubicacion.codigo,
@@ -36,34 +37,38 @@ class Ubicacion
 		      AND clientes_ubicacion.cod_cliente = '$cliente'
 		      ORDER BY 5, 3 DESC ";
 
-    $query = $this->bd->consultar($sql);
+		$query = $this->bd->consultar($sql);
 
-    while ($datos= $this->bd->  obtener_fila($query)) {
-      $this->datos[] = $datos;
-    }
-    return $this->datos;
+		while ($datos = $this->bd->obtener_fila($query)) {
+			$this->datos[] = $datos;
+		}
+		return $this->datos;
 	}
 
-  public function inicio(){
+	public function inicio()
+	{
 
 
-    $this->datos = array('codigo' => '',  'descripcion' => '',
-                         'cod_estado' => '',  'estado' => 'Seleccione...',
-                         'cod_ciudad' => '', 'ciudad' => 'Seleccione...',
-												 'cod_zonas' => '', 'zona' => 'Seleccione...',
-                         'cod_region' => '',   'region' => 'Seleccione...',
-                         'cod_calendario' => '', 'calendario' => 'Seleccione...',
-												 'direccion' => '',  'contacto' => '', 'cargo' => '',  'telefono' => '',
-												 'cod_zona' => '', 'email' => '', 'observacion' => '',
-												 'latitud' => '', 'longitud' => '',
-												   'campo01' => '', 'campo02' => '', 'campo03' => '', 'campo04' => '',
-										     'cod_us_ing' => '', 'fec_us_ing' => '','cod_us_mod' => '', 'fec_us_mod' => '', 'status' => '');
+		$this->datos = array(
+			'codigo' => '',  'descripcion' => '',
+			'cod_estado' => '',  'estado' => 'Seleccione...',
+			'cod_ciudad' => '', 'ciudad' => 'Seleccione...',
+			'cod_zonas' => '', 'zona' => 'Seleccione...',
+			'cod_region' => '',   'region' => 'Seleccione...',
+			'cod_calendario' => '', 'calendario' => 'Seleccione...',
+			'direccion' => '',  'contacto' => '', 'cargo' => '',  'telefono' => '',
+			'cod_zona' => '', 'email' => '', 'observacion' => '',
+			'latitud' => '', 'longitud' => '',
+			'campo01' => '', 'campo02' => '', 'campo03' => '', 'campo04' => '',
+			'cod_us_ing' => '', 'fec_us_ing' => '', 'cod_us_mod' => '', 'fec_us_mod' => '', 'status' => ''
+		);
 		return $this->datos;
-  }
+	}
 
-	public function editar($cod){
+	public function editar($cod)
+	{
 		$this->datos   = array();
-  	$sql = " SELECT clientes_ubicacion.codigo,
+		$sql = " SELECT clientes_ubicacion.codigo,
 										clientes_ubicacion.cod_estado, estados.descripcion  estado,
 						        clientes_ubicacion.cod_ciudad, ciudades.descripcion ciudad,
 	                  clientes_ubicacion.cod_region, regiones.descripcion region,
@@ -85,112 +90,119 @@ class Ubicacion
 								AND clientes_ubicacion.cod_zona = zonas.codigo
 					      AND clientes_ubicacion.codigo = '$cod'";
 
-    $query = $this->bd->consultar($sql);
-    return $this->datos = $this->bd->obtener_fila($query);
-  }
+		$query = $this->bd->consultar($sql);
+		return $this->datos = $this->bd->obtener_fila($query);
+	}
 
-	public function get_cl_nombre($cod){
+	public function get_cl_nombre($cod)
+	{
 		$this->datos   = array();
 		$sql = " SELECT nombre  FROM clientes WHERE codigo = '$cod'";
-    $query = $this->bd->consultar($sql);
-    return $this->datos = $this->bd->obtener_fila($query);
-  }
+		$query = $this->bd->consultar($sql);
+		return $this->datos = $this->bd->obtener_fila($query);
+	}
 
 
-	public function get_concepto($cod){
+	public function get_concepto($cod)
+	{
 		$this->datos   = array();
-	  $sql = " SELECT a.codigo, a.descripcion,  a.abrev
+		$sql = " SELECT a.codigo, a.descripcion,  a.abrev
 	             FROM conceptos AS a
 	            WHERE a.`status` = 'T'
 							  AND a.asist_diaria = 'T'
 	              AND a.codigo <> '$cod'; ";
-    $query = $this->bd->consultar($sql);
+		$query = $this->bd->consultar($sql);
 
-    while ($datos= $this->bd->obtener_fila($query)) {
-      $this->datos[] = $datos;
-    }
-    return $this->datos;
+		while ($datos = $this->bd->obtener_fila($query)) {
+			$this->datos[] = $datos;
+		}
+		return $this->datos;
 	}
-		public function get_region($cod){
+	public function get_region($cod)
+	{
 		$this->datos   = array();
-	  $sql = " SELECT a.codigo, a.descripcion
+		$sql = " SELECT a.codigo, a.descripcion
 	             FROM regiones AS a
 	            WHERE a.`status` = 'T'
 	              AND a.codigo <> '$cod'; ";
-    $query = $this->bd->consultar($sql);
-
-    while ($datos= $this->bd->obtener_fila($query)) {
-      $this->datos[] = $datos;
-    }
-    return $this->datos;
-	}
-
-	public function get_cargo($cod){
-		$this->datos   = array();
-	  $sql = " SELECT a.codigo, a.descripcion
-	             FROM cargos AS a
-	            WHERE a.`status` = 'T'
-	              AND a.codigo <> '$cod'; ";
-    $query = $this->bd->consultar($sql);
-
-    while ($datos= $this->bd->obtener_fila($query)) {
-      $this->datos[] = $datos;
-    }
-    return $this->datos;
-	}
-
-	public function get_zona($cod){
-		$this->datos   = array();
-	  $sql = " SELECT a.codigo, a.descripcion FROM zonas a
-		  				WHERE a.`status` = 'T' AND a.codigo <> '$cod'
-						  ORDER BY 2 ASC ";
-	  $query = $this->bd->consultar($sql);
-
-		while ($datos= $this->bd->obtener_fila($query)) {
-      $this->datos[] = $datos;
-    }
-	  return $this->datos;
-	}
-
-	public function get_calendario($cod){
-		$this->datos   = array();
-		$sql = " SELECT a.codigo, a.descripcion FROM nom_calendario a
-							WHERE a.`status` = 'T' AND a.tipo = 'VAR'  AND a.codigo <> '$cod'
-							ORDER BY 2 ASC ";
 		$query = $this->bd->consultar($sql);
 
-		while ($datos= $this->bd->obtener_fila($query)) {
+		while ($datos = $this->bd->obtener_fila($query)) {
 			$this->datos[] = $datos;
 		}
 		return $this->datos;
 	}
 
-	public function get_estado($cod){
+	public function get_cargo($cod)
+	{
+		$this->datos   = array();
+		$sql = " SELECT a.codigo, a.descripcion
+	             FROM cargos AS a
+	            WHERE a.`status` = 'T'
+	              AND a.codigo <> '$cod'; ";
+		$query = $this->bd->consultar($sql);
+
+		while ($datos = $this->bd->obtener_fila($query)) {
+			$this->datos[] = $datos;
+		}
+		return $this->datos;
+	}
+
+	public function get_zona($cod)
+	{
+		$this->datos   = array();
+		$sql = " SELECT a.codigo, a.descripcion FROM zonas a
+		  				WHERE a.`status` = 'T' AND a.codigo <> '$cod'
+						  ORDER BY 2 ASC ";
+		$query = $this->bd->consultar($sql);
+
+		while ($datos = $this->bd->obtener_fila($query)) {
+			$this->datos[] = $datos;
+		}
+		return $this->datos;
+	}
+
+	public function get_calendario($cod)
+	{
+		$this->datos   = array();
+		$sql = " SELECT a.codigo, a.descripcion FROM nom_calendario a
+							WHERE a.`status` = 'T' 
+							-- AND a.tipo = 'VAR'  
+							AND a.codigo <> '$cod'
+							ORDER BY 2 ASC ";
+		$query = $this->bd->consultar($sql);
+
+		while ($datos = $this->bd->obtener_fila($query)) {
+			$this->datos[] = $datos;
+		}
+		return $this->datos;
+	}
+
+	public function get_estado($cod)
+	{
 		$this->datos   = array();
 		$sql = " SELECT a.codigo, a.descripcion FROM estados a , control
 							WHERE a.cod_pais = control.cod_pais AND a.`status` = 'T'
 							  AND a.codigo <> '$cod' ORDER BY 2 ASC ";
 		$query = $this->bd->consultar($sql);
 
-		while ($datos= $this->bd->obtener_fila($query)) {
+		while ($datos = $this->bd->obtener_fila($query)) {
 			$this->datos[] = $datos;
 		}
 		return $this->datos;
 	}
 
-	public function get_ciudad($cod, $cod_estado){
+	public function get_ciudad($cod, $cod_estado)
+	{
 		$this->datos   = array();
 		$sql = " SELECT a.codigo, a.descripcion FROM ciudades a
 							WHERE a.cod_estado = '$cod_estado' AND a.`status` = 'T'
 							  AND a.codigo <> '$cod' ORDER BY 2 ASC ";
 		$query = $this->bd->consultar($sql);
 
-		while ($datos= $this->bd->obtener_fila($query)) {
+		while ($datos = $this->bd->obtener_fila($query)) {
 			$this->datos[] = $datos;
 		}
 		return $this->datos;
 	}
-
-
 }
-?>
