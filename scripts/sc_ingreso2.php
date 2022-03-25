f00bff0598ea18640acd5c225764c979596c140d
YAVILES <yonathanaviles123@gmail.com>
2022-03-25 14:04:35 -0400

    Update sc_ingreso2.php

--- scripts/sc_ingreso2.php
+++ scripts/sc_ingreso2.php
@@ -164,7 +164,7 @@ $metodo   = $_POST['metodo'];
 				$visita = $_POST['visita_'.$cod_doc.''];
 			  $status_doc     = $_POST['status_'.$cod_doc.''];
 			  $fecha      = $_POST['fec_'.$cod_doc.''] != "" ?  conversion($_POST['fec_'.$cod_doc.'']) : '0000-00-00';
-			
+			echo $_POST['fec_'.$cod_doc.''];
 			 $sql02    = "INSERT INTO chequeos_trab(cedula, codigo, fecha, status, observacion) VALUES ($codigo,'$cod_doc', '$status_doc', '$fecha', '$observacion') 
 			 ON DUPLICATE KEY UPDATE status='$status_doc', observacion='$observacion', fecha='$fecha';";
 			 echo $sql02;
